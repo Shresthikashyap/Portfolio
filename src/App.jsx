b630ecdefac90bca571f66f110388bc335737ae9
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2026-01-05 14:56:28 +0530

    Include Experience component in App layout
    Added Experience component to the main layout.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import SocialLinks from './components/SocialLinks';
@@ -12,9 +13,10 @@ const App = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <SocialLinks />
-      <main className="container mx-auto px-6 md:px-12 lg:px-24 flex-grow">
+      <main className="container  px-6 md:px-12 lg:px-24 flex-grow">
         <Hero />
         <Skills />
+        <Experience />
         <Projects />
         <Contact />
       </main>
